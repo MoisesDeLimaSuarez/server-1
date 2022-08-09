abba320529de3eac39f481bc7f2efc6e08adb567
Moises De Lima <mfdls.dev@gmail.com>
2022-08-09 17:55:11 +0100

    se agrego una bd y funciones de get

--- index.js
+++ index.js
@@ -58,11 +58,71 @@ const dataBase = [
     }
 ]
 
-
+const docentes = [{
+    id: 1,
+    nombre: 'Valka',
+    apellidos: 'Dondarr',
+    dni: '42.556.777-M',
+    email: 'valka@sunmmail.com',
+    password: 'valkita98?=',
+},
+{
+    id: 2,
+    nombre: 'Adrián',
+    apellidos: 'Santos',
+    dni: '43.123.444-B',
+    email: 'adrian@sunmmail.com',
+    password: 'adsan45*!'
+},
+{
+    id: 3,
+    nombre: 'Cristo',
+    apellidos: 'Rey Santos',
+    dni: '42.111.222-C',
+    email: 'inri@sunmmail.com',
+    password: 'inricrist0?¿=',
+},
+{
+    id: 4,
+    nombre: 'Lucifer',
+    apellidos: 'Hell Hell',
+    dni: '66.666.666-A',
+    email: 'lucihell@sunmmail.com',
+    password: '66666666',
+},
+{
+    id: 5,
+    nombre: 'Valdimiro',
+    apellidos: 'Rebollo',
+    dni: '41.544.778-O',
+    email: 'vladi@sunmmail.com',
+    password: 'vladiFuckYou4U,:*',
+},
+{
+    id: 6,
+    nombre: 'Lara',
+    apellidos: 'Sanz',
+    dni: '43.566.622-F',
+    email: 'laraz@sunmmail.com',
+    password: 'larili456',
+},
+];
 
 //? GET /api/users
 
 app.use("/api", rutas);
+app.get("/api/docentes", (req, res) => {
+    res.json(docentes);
+});
+app.get("/api/docentes/:id", (rec, res) => {
+
+    const docente = docentes.find(docente => docente.id === parseInt(rec.params.id));
+    if (!docente) {
+        res.status(404).send("No existe el docente");
+    }
+    res.json(docente);
+
+})
 
 //? crear usuario  /api/users
 
